6ea12a2d9f0a80fe31b8b20b2d589cd39b77fdab
Dinakar <103622152+DINAKAR99@users.noreply.github.com>
2025-06-24 12:10:35 +0530

    Create FirebaseConfig.java

---
+++ notifications/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package in.gov.cgg;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() {
+        try {
+InputStream serviceAccount = getClass().getClassLoader()
+    .getResourceAsStream("test-miv-firebase-adminsdk-fbsvc-9cf82484d3.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
